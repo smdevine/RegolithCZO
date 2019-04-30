67d889714f558a17bdcf594139d9d9102b1d3433
smdevine <smdevine@ucdavis.edu>
2019-04-30 14:35:51 -0700

    few changes to get to Desktop

--- download_landsat8.R
+++ download_landsat8.R
@@ -1,12 +1,12 @@
 #get landsat8 data
-pointsDir <- 'C:/Users/smdevine/Desktop/PostDoc/CZO/sampling pts'
+#pointsDir <- 'C:/Users/smdevine/Desktop/PostDoc/CZO/sampling pts'
 downloadfileDir <- 'C:/Users/smdevine/Desktop/PostDoc/CZO/landsat8'
 # Install the latest version from GitHub:
 install.packages("devtools")
 devtools::install_github("socialcopsdev/rLandsat")
 # Load the library
 library(rLandsat)
-year <- '2015'
+year <- '2017'
 data_source <- landsat_search(min_date = paste0(year, '-01-01'), max_date = paste0(year, '-12-31'), path_master = 42, row_master = 34) #path 42 and row 34 covers soaproot watershed
 dim(data_source)
 colnames(data_source)
@@ -14,11 +14,14 @@ unique(data_source$product_id)
 espa_creds('smdevine', 'Cienciasoil80')
 test <- espa_products(data_source$product_id)
 test$master #1's indicate availability
+dim(test$master)
+test$master$sr_ndvi
 
 # placing an espa order
-result_order <- espa_order(input_ids = data_source$product_id, product = c("sr_ndvi", "sr_evi"), projection = "lonlat", order_note = "Soaproot coverage 2014") #including source data "sr" and "toa" requires too much memory
+result_order <- espa_order(input_ids = data_source$product_id, product = c("sr_ndvi", "sr_evi"), projection = "lonlat", order_note = paste0("Soaproot cover", year)) #including source data "sr" and "toa" requires too much memory
 order_id <- result_order$order_details$orderid
-
+#for 2016 order: "espa-smdevine@ucdavis.edu-04302019-125550-891"
+#for 2017 order: "espa-smdevine@ucdavis.edu-04302019-131936-862"
 # getting order status
 durl <- espa_status(order_id = order_id, getSize = FALSE) #session times out if getSize set to TRUE after files become available
 downurl <- durl$order_details
