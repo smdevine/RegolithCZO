bb10db04d2e2164eaa5886b4a96f126371f57e48
smdevine <smdevine@ucdavis.edu>
2019-04-30 09:53:46 -0700

    few minor changes to understand flowacc distribution

--- soaproot_flowacc.R
+++ soaproot_flowacc.R
@@ -1,10 +1,12 @@
 library(raster)
-flowacc <- raster('C:/Users/smdevine/Desktop/PostDoc/CZO/NEON/DEM/10m_filled/flowacc_10m.tif')
+#flowacc <- raster('C:/Users/smdevine/Desktop/PostDoc/CZO/NEON/DEM/10m_filled/flowacc_10m.tif')
+flowacc <- raster('C:/Users/smdevine/Desktop/PostDoc/CZO/NEON/DEM/5m_filled/flowacc_5m.tif')
 flowacc
-hist(flowacc$flowacc_10m[flowacc$flowacc_10m<1000])
+hist(flowacc$flowacc_5m[flowacc$flowacc_5m<1000])
 freq_table <- freq(flowacc)
 dim(freq_table)
 tail(freq_table, 100)
+head(freq_table, 200)
 freq_table[400,]
 sum(freq_table[1:400,2]) #116439 out of 120000 are <400
 sum(freq_table[1:150,2]) #114206 out of 120000 are <150
