c2a7306e154c45629557cc48f1eef0dd31487084
smdevine <smdevine@ucdavis.edu>
2019-08-16 06:12:39 -0700

    got rid of superfluous material and added violin plot exports

--- figures/total_chemistry.R
+++ figures/total_chemistry.R
@@ -2,47 +2,15 @@ library(raster)
 dataDir <- 'C:/Users/smdevine/Desktop/post doc/czo work'
 landsat8Dir <- 'C:/Users/smdevine/Desktop/post doc/czo work/landsat8/summaries/finals'
 DSDdir <- 'C:/Users/smdevine/Desktop/post doc/czo work/DSD Mike'
-df_master <- read.csv(file.path(dataDir, 'Master Data Set_with rock outcrop.csv'), stringsAsFactors = FALSE) #removed biological data from this dataset but it has duplicate rows where multiple biological observations were made at each point; also removed 0's from outcrop (OC) site codes 01-09 to match points dataset labeling
-df_sites <- df_master[match(unique(df_master$Site), df_master$Site), ]
-soaproot_pts <- read.csv(file.path(dataDir, 'Soaproot points RF.csv'), stringsAsFactors = FALSE)
-soaproot_pts_WGS84 <- SpatialPointsDataFrame(coords=soaproot_pts[,c('POINT_X', 'POINT_Y')], data = soaproot_pts['Name'], proj4string = CRS('+init=epsg:4326 +proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0'))
-soaproot_UTM11N_shp <- spTransform(soaproot_pts_WGS84, CRS("+proj=utm +zone=11 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")) #project from geographic to WGS84 UTM 11N
-resolution <- '5m'
-if(resolution == '10m') {NEONterrainDir <- 'C:/Users/smdevine/Desktop/post doc/czo work/NEON 10m/terrain characteristics'
-} else if(resolution == '5m') {NEONterrainDir <- 'C:/Users/smdevine/Desktop/post doc/czo work/5m terrain characteristics/5m filtered'} #5m filtered data produced from 7/12/19 arcgis work
-NEON_terrain <- stack(list.files(NEONterrainDir, full.names = TRUE))
-if(resolution == '10m') {
-  NEON_terrain$annsolrad_10m <- NEON_terrain$annsolrad_10m / 1000
-  names(NEON_terrain)
-  names(NEON_terrain) <- c('solrad_N', 'aspect_N', 'CTI_N', 'curv_mean_N', 'curv_plan_N', 'curv_prof_N', 'elev_N', 'elev_above_str_150', 'EVI_2017_N', 'EVI_2018_N', 'flowacc_N', 'NDVI_2017_N', 'NDVI_2018_N', 'SEI_N', 'slope_N', 'stream_dist_N_100', 'stream_dist_N_150', 'stream_dist_N_200', 'stream_dist_N_300', 'stream_dist_N_400', 'TCI_N')
-} else if(resolution == '5m') {
-  NEON_terrain$solrad_5m <- NEON_terrain$solrad_5m / 1000
-  names(NEON_terrain)
-  names(NEON_terrain) <- c('aspect_N', 'CTI_N', 'curv_mean_N', 'curv_plan_N', 'curv_prof_N', 'elev_N', 'elev_above_str_N', 'EVI_2017_N', 'EVI_2018_N', 'IMI_N', 'NDVI_2017_N', 'NDVI_2018_N', 'SEI_N', 'slope_N', 'solrad_N', 'stream_dist_N')
-}
-NEON_terrain$TPI_N <- terrain(NEON_terrain$elev_N, opt = 'TPI', neighbors = 8)
-NEON_terrain$TRI_N <- terrain(NEON_terrain$elev_N, opt = 'TRI', neighbors = 8)
-DSD_v2 <- raster(file.path(DSDdir, 'DSDout_V2_1_cropped.tif')) #cropped version derived from DSDout_V2_1.tif'
-
-soaproot_pts_terrain <- extract(NEON_terrain, soaproot_UTM11N_shp, df=TRUE)
-soaproot_pts_terrain$ID <- NULL
-soaproot_pts_terrain <- cbind(soaproot_UTM11N_shp$Name, soaproot_pts_terrain)
-colnames(soaproot_pts_terrain)[1] <- 'Site'
-soaproot_pts_terrain$Site <- as.character(soaproot_pts_terrain$Site)
-soaproot_pts_terrain$DSD_v2_M <- extract(DSD_v2, soaproot_pts_WGS84) / 10
-soaproot_pts_analysis <- soaproot_pts_terrain[grepl('SR.A.', soaproot_pts_terrain$Site), ] #leaves out all rock outcrop points
-soaproot_pts_analysis$Depth <- df_sites$Depth[match(soaproot_pts_analysis$Site, df_sites$Site)]
-soaproot_pts_analysis <- soaproot_pts_analysis[!is.na(soaproot_pts_analysis$Depth),] #one point had a NA for depth
-soaproot_pts_analysis$depth_class <- as.factor(ifelse(soaproot_pts_analysis$Depth < 3.3, 1, ifelse(soaproot_pts_analysis$Depth < 7.56, 2, 3))) #1=shallow; 2=moderate; 3=deep
-soaproot_pts_analysis$depth_class_2 <- as.factor(ifelse(soaproot_pts_analysis$Depth < 5.615, 1, 2)) #1=shallow-moderate; 2=deep
-colnames(soaproot_pts_analysis)
-
+FiguresDir <- 'C:/Users/smdevine/Desktop/post doc/czo work/results/figures'
+#use read-in_data.R to get other necessary soaproot data
 list.files(file.path(dataDir, 'tina paper'))
 k2o_df <- read.csv(file.path(dataDir, 'tina paper', 'k2o_data.csv'), stringsAsFactors = FALSE, na.strings = 'L.N.R.')
 
 dim(k2o_df)
 head(k2o_df)
 length(unique(k2o_df$Site)) #58
+table(k2o_df$Site)
 class(k2o_df$K2O_.)
 # k2o_df$K2O_. <- as.numeric(k2o_df$K2O_.)
 tapply(k2o_df$K2O_., k2o_df$Site, mean, na.rm = TRUE)
@@ -78,6 +46,7 @@ aggregate_andsome <- function(x, y) {
   result <- aggregate(y[[x]] ~ y[['Site']], FUN = mean, na.rm = TRUE)
   colnames(result) <- c('Site', x)
   result$Depth <- soaproot_pts_analysis$Depth[match(result$Site, soaproot_pts_analysis$Site)]
+  result$depth_class <- soaproot_pts_analysis$depth_class[match(result$Site, soaproot_pts_analysis$Site)]
   result
 }
 si_to_al_means <- aggregate_andsome('Si.Al', tot_elem_df)
@@ -100,16 +69,12 @@ summary(lm(SiO2 ~ Depth, data = si_means))
 ca_means <- aggregate_andsome('CaO', tot_elem_df)
 plot(ca_means$Depth, ca_means$CaO)
 summary(lm(CaO ~ Depth, data = ca_means))
-tiff(file = file.path(FiguresDir, 'stream_dist_vioplots.tif'), family = 'Times New Roman', pointsize = 11, width = 4.5, height = 3.5, units = 'in', res=res_plots, compression = 'lzw')
-par(mar=c(4.5, 4.5, 0.5, 0.5))
-vioplot(soaproot_pts_analysis$str_dist[soaproot_pts_analysis$depth_class==1], soaproot_pts_analysis$str_dist[soaproot_pts_analysis$depth_class==2], soaproot_pts_analysis$str_dist[soaproot_pts_analysis$depth_class==3], xlab=c('Regolith depth class (m)'), ylab='Distance from channel (m)', names=c('Shallow <3.3', 'Moderate 3.3-7.5', 'Deep >7.5'))
-dev.off()
-
 
 mg_means <- aggregate_andsome('MgO', tot_elem_df)
 plot(mg_means$Depth, mg_means$MgO)
 text(mg_means$Depth, mg_means$MgO, mg_means$Site, pos = 1, offset = 0.5)
 summary(lm(MgO ~ Depth, data = mg_means[mg_means$Site != 'SR.A.51' & mg_means$Site != 'SR.A.55',])) #2 outliers: 51 and 55
+mg_means <- mg_means[mg_means$Site != 'SR.A.51' & mg_means$Site != 'SR.A.55',]
 
 na_means <- aggregate_andsome('Na2O', tot_elem_df)
 plot(na_means$Depth, na_means$Na2O)
@@ -117,3 +82,21 @@ summary(lm(Na2O ~ Depth, data = na_means))
 
 elem_by_site <- data.frame(Site=ca_means$Site, Depth=ca_means$Depth, CaO=ca_means$CaO, MgO=mg_means$MgO, K2O=k_means$K2O, Na2O=na_means$Na2O, Al2O3=al_means$Al2O3, SiO2=si_means$SiO2)
 
+
+tiff(file = file.path(FiguresDir, 'total chemistry', 'Ca_vioplots.tif'), family = 'Times New Roman', pointsize = 11, width = 4.5, height = 3.5, units = 'in', res=res_plots, compression = 'lzw')
+par(mar=c(4.5, 4.5, 0.5, 0.5))
+vioplot(ca_means$CaO[ca_means$depth_class==1], ca_means$CaO[ca_means$depth_class==2], ca_means$CaO[ca_means$depth_class==3], xlab='Regolith depth class (m)', ylab='Calcium oxide (%)', names=c('Shallow <3.3', 'Moderate 3.3-7.5', 'Deep >7.5'))
+dev.off()
+
+plot_the_vioplot <- function(fname, df, varname, ylabel) {
+  tiff(file = file.path(FiguresDir, 'total chemistry', fname), family = 'Times New Roman', pointsize = 11, width = 4.5, height = 3.5, units = 'in', res=res_plots, compression = 'lzw')
+  par(mar=c(4.5, 4.5, 0.5, 0.5))
+  vioplot(df[[varname]][df$depth_class==1], df[[varname]][df$depth_class==2], df[[varname]][df$depth_class==3], xlab='Regolith depth class (m)', ylab=ylabel, names=c('Shallow <3.3', 'Moderate 3.3-7.5', 'Deep >7.5'))
+  dev.off()
+}
+#expression('title'[2]
+plot_the_vioplot('CaO_vioplots.tif', ca_means, 'CaO', 'CaO (%)')
+plot_the_vioplot('K2O_vioplots.tif', k_means, 'K2O', expression('K'[2]*'O (%)'))
+plot_the_vioplot('MgO_vioplots.tif', mg_means, 'MgO', 'MgO (%)')
+plot_the_vioplot('Na2O_vioplots.tif', na_means, 'Na2O', expression('Na'[2]*'O (%)'))
+plot_the_vioplot('Al2O3_vioplots.tif', al_means, 'Al2O3', expression('Al'[2]*'O'[3]~'(%)'))
