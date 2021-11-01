224a6ef9cf1f914f1e3125ce0860f02d704196b2
smdevine <smdevine@ucdavis.edu>
2021-11-01 14:05:33 -0700

    revised figures for pub

--- figures/cdf_curves.R
+++ figures/cdf_curves.R
@@ -1,12 +1,17 @@
 DataFitDir <- 'C:/Users/smdevine/Desktop/post doc/czo work/fit data distribution'
+FiguresDir <- 'C:/Users/smdevine/Desktop/post doc/czo work/results/figures'
+library(extrafont)
+library(extrafontdb)
+#font_import() only needs to be done once
+loadfonts()
 list.files(DataFitDir)
-data_fits <- read.csv(file.path(DataFitDir, 'czo regolith data dist fits.csv'), stringsAsFactors = FALSE) #this analysis was completed in " .xlsx"
+data_fits <- read.csv(file.path(DataFitDir, 'czo regolith data dist fits_revised.csv'), stringsAsFactors = FALSE) #this analysis was completed in " .xlsx"
 head(data_fits)
-
+res_plots <- 800
 gray_colors <- gray.colors(n=4, start = 0.05, end = 0.75)
 lwd_setting <- 2
 
-tiff(file = file.path(FiguresDir, 'cdf0_8_data_distributions.tif'), family = 'Times New Roman', pointsize = 11, width = 3.25, height = 3.5, units = 'in', res=res_plots, compression = 'lzw')
+tiff(file = file.path(FiguresDir, 'cdf0_8_data_distributions_revised.tif'), family = 'Times New Roman', pointsize = 11, width = 3.25, height = 3.5, units = 'in', res=res_plots, compression = 'lzw')
 par(mar=c(4.5, 4.5, 0.5, 0.5))
 plot(data_fits$Depth, data_fits$observed.plotting.position, type='p', pch=16, col=gray_colors[1], xlim=c(0,8), ylim=c(0,0.58), ylab='Cumulative probability', xlab='Regolith depth (m)')
 lines(data_fits$Gamma.prediction, data_fits$probability, lty=2, lwd=lwd_setting, col=gray_colors[2])
@@ -17,7 +22,7 @@ lines(data_fits$Normal, data_fits$probability, lty=5, lwd=lwd_setting, col=gray_
 text(0.16,0.55, 'b', adj=c(0,0))
 dev.off()
 
-tiff(file = file.path(FiguresDir, 'cdf0_40_data_distributions.tif'), family = 'Times New Roman', pointsize = 11, width = 3.25, height = 3.5, units = 'in', res=res_plots, compression = 'lzw')
+tiff(file = file.path(FiguresDir, 'cdf0_40_data_distributions_revised.tif'), family = 'Times New Roman', pointsize = 11, width = 3.25, height = 3.5, units = 'in', res=res_plots, compression = 'lzw')
 par(mar=c(4.5, 4.5, 0.5, 0.5))
 plot(data_fits$Depth, data_fits$observed.plotting.position, type='p', pch=16, cex=0.6, col=gray_colors[1], xlim=c(0,40), ylim=c(0,0.99), ylab='Cumulative probability', xlab='Regolith depth (m)')
 lines(data_fits$Gamma.prediction, data_fits$probability, lty=2, lwd=lwd_setting, col=gray_colors[2])
